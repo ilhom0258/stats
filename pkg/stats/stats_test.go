7ed88033382f220be70828188acc1cc3c0d697b6
ilhom0258 <ilhom0258@gmail.com>
2020-09-26 23:30:42 +0500

    Implemented feature/CategoriesAvg with error test

--- pkg/stats/stats_test.go
+++ pkg/stats/stats_test.go
@@ -28,3 +28,24 @@ func TestCategoriesAvg_success(t *testing.T) {
 		t.Errorf("invalid result, expected %v, actual %v", expected, result)
 	}
 }
+func TestCategoriesAvg_error(t *testing.T) {
+	payments := []types.Payment{
+		{ID: 1, Category: "auto", Amount: 1_000_00},
+		{ID: 2, Category: "food", Amount: 1_000_00},
+		{ID: 3, Category: "auto", Amount: 1_000_00},
+		{ID: 4, Category: "school", Amount: 1_000_00},
+		{ID: 5, Category: "fun", Amount: 1_000_00},
+		{ID: 6, Category: "food", Amount: 1_000_00},
+		{ID: 7, Category: "school", Amount: 1_000_00},
+	}
+	expected := map[types.Category]types.Money{
+		"auto":   100000,
+		"food":   100000,
+		"school": 100000,
+		"fun":    200000,
+	}
+	result := CategoriesAvg(payments)
+	if !reflect.DeepEqual(expected, result) {
+		t.Errorf("invalid result, expected %v, actual %v", expected, result)
+	}
+}
