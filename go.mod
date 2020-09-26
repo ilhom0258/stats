4c6ce3f476715cc81e752cb2276a5c2ad73b3a0c
ilhom0258 <ilhom0258@gmail.com>
2020-09-26 18:40:35 +0500

    Last commit

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/ilhom0258/stats/v2
 
 go 1.15
 
-require github.com/ilhom0258/bank/v2 v2.0.0-20200926123940-8c85c66e38ed
+require github.com/ilhom0258/bank/v2 v2.0.0
--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -19,7 +19,6 @@ func TotalInCategory(payments []types.Payment, category types.Category) types.Mo
 		if strings.ToLower(paymentCategoryValue) != strings.ToLower(searchCategory) {
 			continue
 		}
-
 		if payment.Status == types.StatusFail {
 			continue
 		}
@@ -33,12 +32,15 @@ func TotalInCategory(payments []types.Payment, category types.Category) types.Mo
 //Avg - рассчитывает среднюю сумму платежа
 func Avg(payments []types.Payment) types.Money {
 	var avgPayment types.Money = types.Money(0)
+	var amountPayment types.Money = types.Money(0)
 	for _, payment := range payments {
-		if payment.Status == types.StatusFail {
+		if payment.Status == types.StatusFail{
 			continue
 		}
+		amountPayment ++ 
 		avgPayment += payment.Amount
 	}
-	avgPayment /= types.Money(len(payments))
+
+	avgPayment /= amountPayment
 	return avgPayment
 }
